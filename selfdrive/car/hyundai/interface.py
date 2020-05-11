559b70b2c003e9d633f2507db2b7177841d9afdd
multikyd <multikyd@gmail.com>
2020-05-11 13:56:32 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -249,8 +249,8 @@ class CarInterface(CarInterfaceBase):
     # most HKG cars has no long control, it is safer and easier to engage by main on
     ret.cruiseState.enabled = ret.cruiseState.available if not self.CC.longcontrol else ret.cruiseState.enabled
 
-    ret.leftBlinker = self.CS.left_blinker_flash or self.CS.prev_left_blinker and self.CC.turning_signal_timer
-    ret.rightBlinker = self.CS.right_blinker_flash or self.CS.prev_right_blinker and self.CC.turning_signal_timer
+    ret.leftBlinker = True if ( self.CS.left_blinker_flash or self.CS.prev_left_blinker and self.CC.turning_signal_timer ) else False
+    ret.rightBlinker = True if (self.CS.right_blinker_flash or self.CS.prev_right_blinker and self.CC.turning_signal_timer ) else False
 
     # turning indicator alert logic
     if (ret.leftBlinker or ret.rightBlinker or self.CC.turning_signal_timer) and ret.vEgo < 16.666667:
